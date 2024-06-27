c03679c5486e1f7b68b21b0f06529a1428cac860
tattapan <tattapanwan@ever.app>
2024-06-27 21:50:48 +0700

    setup:testweb

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-<h1 className="text-4xl">C-Food&Frozen</h1>
+    <>
+    <div className="flex  flex-col items-center justify-center h-screen">
+
+    <h1 className="text-4xl font-extrabold">C-Food&Frozen</h1>
+
+<h1 className="text-3xl font-semibold">สำหรับทดสอบระบบ</h1>
+
+    </div>
+
+
+    </>
+
+
   );
 }
