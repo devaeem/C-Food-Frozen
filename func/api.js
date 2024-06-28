3c6c159301b7858d10771e07d3e6b81cb6f474c3
tattapan <tattapanwan@ever.app>
2024-06-29 00:42:53 +0700

    fix:path api

--- func/api.js
+++ func/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export const getCategories = async () =>
-  await axios.get("http://localhost:3000/api/category");
+  await axios.get("https://cfoodfrozen.vercel.app/api/category");
 
 
 export const createCategories = async (payload) =>
- await axios.post("http://localhost:3000/api/category",payload);
+ await axios.post("https://cfoodfrozen.vercel.app/api/category",payload);
+ // await axios.post("http://localhost:3000/api/category",payload);
