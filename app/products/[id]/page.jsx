1ac27096cdaaec4012584e5e8e7268eaf5559991
tattapan <tattapanwan@ever.app>
2024-07-06 15:30:34 +0700

    fix rat produvt

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -1,15 +1,28 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 import Header from "../../components/header";
 import Link from "next/link";
 
-import { getProductId } from "../../../func/productapi";
+import { getProductId, randomProduct } from "../../../func/productapi";
 import CarouselComponent from "../../components/CarouselComponent";
 const Page = () => {
   const params = useParams();
   const { id } = params;
+  const [random, setRandom] = useState([]);
+
+  const { data: listRandomProducts } = useQuery({
+    queryKey: ["list-random-product"],
+    queryFn: async () => {
+      try {
+        const res = await randomProduct();
+        return res.data.products;
+      } catch (err) {
+        throw err;
+      }
+    },
+  });
 
   const { data: listProductGet } = useQuery({
     queryKey: ["list-get-productId-show", { id }],
@@ -23,6 +36,20 @@ const Page = () => {
       }
     },
   });
+
+  useEffect(() => {
+    resData();
+  }, [id]);
+
+  const resData = () => {
+    randomProduct()
+      .then((res) => {
+        setRandom(res.data.products);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  };
 
   const products = [
     {
@@ -90,61 +117,28 @@ const Page = () => {
         </div>
       </div>
 
-      {/* <div className="mt-8 md:mt-8 lg:mt-4 md:container md:mx-auto lg:container lg:mx-auto">
-      <div className="max-w-7xl mx-auto bg-white rounded-lg shadow-lg overflow-hidden">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-8">
-
-          <div>
-            <CarouselComponent productData={productData} />
-          </div>
-
-
-          <div className="flex flex-col justify-center">
-            <h1 className="text-gray-900 font-bold text-4xl mb-4">
-              {productData.name}
-            </h1>
-            <h4 className="text-gray-400 font-semibold text-xl mb-3">
-              รหัสสินค้า: {id}
-            </h4>
-            <h4 className="text-gray-500 font-semibold text-lg mb-3">
-              หมวดหมู่สินค้า:{" "}
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                {productData.categoryRef?.name}
-              </span>
-            </h4>
-
-            <p className="text-gray-600 text-lg mb-6">{productData.desc}</p>
-            <p className="text-gray-900 font-bold text-3xl mb-6">
-              ฿{productData.price}
-            </p>
-            <button className="px-6 py-3 bg-blue-500 text-white text-lg font-bold rounded hover:bg-blue-700 focus:outline-none focus:bg-blue-700">
-              สนใจสินค้านี้
-            </button>
-          </div>
-        </div>
-      </div>
-    </div> */}
-
       <div className="container mx-auto mt-8">
         <h2 className="text-2xl font-semibold mt-2 py-8">Related Products</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {products.map((product) => (
+          {listRandomProducts?.map((product) => (
             <div
-              key={product.id}
+              key={product?.id}
               className="bg-white shadow-md rounded-lg overflow-hidden"
             >
-              <Link href={`/products/${product.id}`}>
+              <Link href={`/products/${product?.id}`}>
                 <div>
                   <img
                     className="w-full h-64 object-cover"
-                    src={product.imageUrl}
-                    alt={product.name}
+                    src={product?.images[0]?.url}
+                    alt={product?.name}
                   />
                   <div className="p-4">
                     <h2 className="text-gray-900 font-bold text-xl">
-                      {product.name}
+                      {product?.name}
                     </h2>
-                    <p className="mt-2 text-gray-600">Price: {product.price}</p>
+                    <p className="mt-2 text-gray-600">
+                      Price: {product?.price}
+                    </p>
                   </div>
                 </div>
               </Link>
--- func/productapi.js
+++ func/productapi.js
@@ -11,35 +11,39 @@ export const getProduct = async (search, page, pageSize, categoryId) => {
   return await axios.get(url, { params });
 };
 
-export const createProduct  = async ({token,payload}) =>
-  await axios.post(process.env.NEXT_PUBLIC_API + "/product", payload,{
+export const createProduct = async ({ token, payload }) =>
+  await axios.post(process.env.NEXT_PUBLIC_API + "/product", payload, {
     headers: {
-      authtoken:token,
+      authtoken: token,
     },
   });
 
+export const randomProduct = async () => {
+  const url = process.env.NEXT_PUBLIC_API + "/productrandom";
 
+  return await axios.get(url);
+};
 
 export const getProductId = async (id) => {
   return await axios.get(process.env.NEXT_PUBLIC_API + `/product/${id}`);
 };
 
-export const updateProductId = async ({token,editId, payload}) => {
-
+export const updateProductId = async ({ token, editId, payload }) => {
   return await axios.put(
     process.env.NEXT_PUBLIC_API + `/product/${editId}`,
-    payload,{
+    payload,
+    {
       headers: {
-        authtoken:token,
+        authtoken: token,
       },
     }
   );
 };
 
-export const delProductId = async ({token,id}) => {
-  return await axios.delete(process.env.NEXT_PUBLIC_API + `/product/${id}`,{
+export const delProductId = async ({ token, id }) => {
+  return await axios.delete(process.env.NEXT_PUBLIC_API + `/product/${id}`, {
     headers: {
-      authtoken:token,
+      authtoken: token,
     },
   });
 };
